d4b32956edbcd3ae15c799d25ff49e420fd0fce6
Aaron Fink <apf75@cornell.edu>
2021-03-11 15:37:07 -0500

    first commit

---
+++ arrays.js
@@ -0,0 +1,80 @@
+// As you may know, an anagram is a new word created by reorganizing all the letters in a word.
+// This array of anagrams are all based on the word "Stop"
+let anagrams = ["plots", "post", "stop", "puts", "tops"]
+
+// Run this program after you try each step and see if it worked.
+// At the very bottom of this file is a line of code that prints out the array after your code is run.
+// You're welcome to print additional debugging statements if you need.
+
+// 1. Print out the third word in the array using bracket notation.
+
+
+
+
+
+// 2. The word "opts" is also an anagram of the word "stop". Find a way to add "opts" to the end of the array.
+
+
+
+
+
+// 3. The word "puts", on the other hand, is not an anagram of the word "stop". Find a way to replace it with the word "pots".
+
+
+
+
+
+// 4. Use the documentation to figure out what the method ".shift()" does.
+//   Now use it to remove the word "plots" (which isn't a correct anagram of "stop") from our array.
+
+
+
+
+
+// 5. Put the final array of anagrams in alphabetical order.
+
+
+
+
+// This code prints the array after you've manipulated it above.
+console.log(anagrams)
+
+// LEVEL 2: At this point, the tasks will get more challenging, because you'll work with datasets too large to handle by just looking at it and reading it.
+// The arrays used in the second half of this lab will be stored in a neighboring file called "other_lists.js".
+// If you'd like to look at that file, you certainly may, but do your work here.
+// This line of code connects the data.
+const other_lists = require('./other_lists')
+
+// Pro-tip: to make this easier, comment out the line "console.log(anagrams)" above so that you aren't printing extra information.
+//
+//     The first array is called "fortunes" and contains fortune-cookie style fortunes.
+//     Remeber to access the array with "other_lists.fortunes" because is is in a different file.
+//
+//  6. Print out the 10th fourtune from the array.
+
+console.log(other_lists.fortunes[9])
+
+
+//  7. Print out fortunes 12, 13, and 14 from the array. There are several ways to do this, and any way is totally fine.
+
+
+
+// 8. This list is huge, so it'd be helpful to know how many fortunes are listed. Find a way to print out the number of fortunes in the array.
+
+
+
+// 9. Print out a random fortune from the array.
+//
+//     You're going to need to use Math.random() and Math.floor(). See if you can figure out how they work here: https://www.w3schools.com/js/js_random.asp
+//     Then think about what range you need to choose a random number from.
+
+
+
+
+
+
+
+
+// 10. Challenge: Out of all the fotunes that are there, it'd mess up the program if some were listed twice, but with an array that big, it could happen.
+// Find a way to check and see whether any of the fortunes are duplicates - make a separate array of those fortunes.
+// Once you have the array of duplicated fortunes, find a way to delete those duplicates.
