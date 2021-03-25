1b3815fff7a7127a52445cd0e74cdb017476a31c
Aaron <aaronpfink@gmail.com>
2021-03-25 02:18:25 -0400

    removed requires (index handles it)

--- arrays.js
+++ arrays.js
@@ -42,13 +42,10 @@ console.log(anagrams)
 // LEVEL 2: At this point, the tasks will get more challenging, because you'll work with datasets too large to handle by just looking at it and reading it.
 // The arrays used in the second half of this lab will be stored in a neighboring file called "otherArrays.js".
 // If you'd like to look at that file, you certainly may, but do your work here.
-// This line of code connects the data.
-const otherArrays = require('./otherArrays')
 
 // Pro-tip: to make this easier, comment out the line "console.log(anagrams)" above so that you aren't printing extra information.
 //
 //     The first array is called "fortunes" and contains fortune-cookie style fortunes.
-//     Remeber to access the array with "otherArrays.fortunes" because is is in a different file.
 //
 //  6. Print out the 10th fourtune from the array.
 
